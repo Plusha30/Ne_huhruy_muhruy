f2775ea05f29ca4d02c2dbc761f9f2761a15ed71
Plusha30 <plushasstudio@gmail.com>
2026-01-22 15:51:07 +0300

    made my database program in other file

--- promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/app.py
+++ promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/app.py
@@ -1,28 +1,9 @@
 from flask import Flask, render_template
-import os
-import json
-import pathlib
-import shutil
+from ivan_db import *
 
 app = Flask(__name__)
 app.secret_key = 'hackathon_key'
-tovars_data = {}
-users_base = {}
-email = 'placeholder'
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
-#mdkasmdlaskmdlam
-
-def return_image(path, placeholder):
-    if os.path.exists(f"{pathlib.Path(__file__).parent.resolve()}/static/images/{path}.jpg"):
-        return f'images/{path}.jpg'
-    else:
-        return f'images/common/{placeholder}.jpg'
-
-def commonkwargs(kwargs):
-    if (email in users_base):
-        return kwargs | {'username': users_base[email][1], 'userimg': return_image(f'users/{email}', 'user_placeholder')}
-    else:
-        return kwargs | {'username': 'Вы', 'userimg': return_image(f'users/{email}', 'user_placeholder')}
 
 @app.route('/')
 def landing():
@@ -30,29 +11,33 @@ def landing():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', **commonkwargs({}))
 
 @app.route('/object/<int:id>')
 def object_detail(id):
-    return render_template('object.html', id=id)
+    return render_template('object.html', id=id, **commonkwargs)
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', **commonkwargs({}))
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('register.html', **commonkwargs({}))
 
 # НОВЫЙ МАРШРУТ
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', **commonkwargs({}))
 
 @app.route('/pricing')
 def pricing():
     # ТЕПЕРЬ МЫ ИСПОЛЬЗУЕМ НОВЫЙ ШАБЛОН
-    return render_template('pricing.html')
+    return render_template('pricing.html', **commonkwargs({}))
 
 if __name__ == '__main__':
-    app.run(debug=True, port="5723")
+    tovars_data = getTovarsData(f"{pathlib.Path(__file__).parent.resolve()}/categories")
+    with open(f"{pathlib.Path(__file__).parent.resolve()}/users_base.json", 'r', encoding='utf-8') as f:
+        info = f.read()
+        users_base = json.loads(info)
+    app.run(port=5237, host="127.0.0.1", debug=True)
---
+++ promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/ivan_db.py
@@ -0,0 +1,40 @@
+import os
+import json
+import pathlib
+import shutil
+
+tovars_data = {}
+users_base = {}
+email = 'placeholder'
+
+def return_image(path, placeholder):
+    if os.path.exists(f"{pathlib.Path(__file__).parent.resolve()}/static/images/{path}.jpg"):
+        return f'images/{path}.jpg'
+    else:
+        return f'images/common/{placeholder}.jpg'
+
+def commonkwargs(kwargs):
+    if (email in users_base):
+        return kwargs | {'username': users_base[email][1], 'userimg': return_image(f'users/{email}', 'user_placeholder')}
+    else:
+        return kwargs | {'username': 'Вы', 'userimg': return_image(f'users/{email}', 'user_placeholder')}
+
+def getTovarsData(folder_path):
+    total = dict()
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+            d = json.loads(content)
+            total |= d
+            for i in d:
+                d[i] |= {"tovars": {}}
+            way = f"{pathlib.Path(__file__).parent.resolve()}/tovars/{filename[:-5]}"
+            for tovar in os.listdir(way):
+                tovar_path = os.path.join(way, tovar)
+                with open(tovar_path, 'r', encoding='utf-8') as f1:
+                    info = f1.read()
+                    tov = json.loads(info)
+                    for i in d:
+                        d[i]["tovars"] |= tov
+    return total
