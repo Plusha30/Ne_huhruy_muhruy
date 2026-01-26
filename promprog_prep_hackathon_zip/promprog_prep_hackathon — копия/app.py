b391da07a9c95f6ae40f0b9309cd0f8054acf87c
Plusha30 <plushasstudio@gmail.com>
2026-01-26 11:58:45 +0300

    fix

--- promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/app.py
+++ promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from subscript.filework import *
-from subscript.reports import *
+#from subscript.reports import *
 import os
 
 #global variables
